8b1c1d5fef497bb86a9277b6c8a786f071c7783f
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2023-07-13 19:04:11 +0530

    initial changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GroupCardComponent } from './feature-component/group-card/group-card.component';
+import { LibraryCardComponent } from './feature-component/library-card/library-card.component';
+import { LibraryCardV2Component } from './feature-component/library-card-v2/library-card-v2.component';
+import { LibraryCardV3Component } from './feature-component/library-card-v3/library-card-v3.component';
+import { LibraryCardV4Component } from './feature-component/library-card-v4/library-card-v4.component';
+import { MemberCardComponent } from './feature-component/member-card/member-card.component';
+import { CourseCardComponent } from './feature-component/course-card/course-card.component';
+import { CourseCuriculumCardComponent } from './feature-component/course-curiculum-card/course-curiculum-card.component';
+import { HomeComponent } from './feature-component/home/home.component';
+import { MyCourseCardComponent } from './feature-component/my-course-card/my-course-card.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    // HomeComponent,
+    // GroupCardComponent
+  ],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'group-card', component: GroupCardComponent },
+      { path: 'library-card', component: LibraryCardComponent },
+      { path: 'library-card-v2', component: LibraryCardV2Component },
+      { path: 'library-card-v3', component: LibraryCardV3Component },
+      { path: 'library-card-v4', component: LibraryCardV4Component },
+      { path: 'member-card', component: MemberCardComponent },
+      { path: 'course-card', component: CourseCardComponent },
+      { path: 'course-curiculum-card', component: CourseCuriculumCardComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'my-course-card', component: MyCourseCardComponent },
+      { path: '**', redirectTo: 'home' },
+
+    ])
+  ],
+  exports: [
+    RouterModule,
+  ],
+  providers: [],
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonConsumptionModule} from 'common-consumption-bootcamp';
+import { HomeComponent } from './feature-component/home/home.component';
+import { GroupCardComponent } from './feature-component/group-card/group-card.component';
+import { LibraryCardComponent } from './feature-component/library-card/library-card.component';
+import { LibraryCardV2Component } from './feature-component/library-card-v2/library-card-v2.component';
+import { LibraryCardV3Component } from './feature-component/library-card-v3/library-card-v3.component';
+import { LibraryCardV4Component } from './feature-component/library-card-v4/library-card-v4.component';
+import { MemberCardComponent } from './feature-component/member-card/member-card.component';
+import { CourseCardComponent } from './feature-component/course-card/course-card.component';
+import { CourseCuriculumCardComponent } from './feature-component/course-curiculum-card/course-curiculum-card.component';
+import { MyCourseCardComponent } from './feature-component/my-course-card/my-course-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GroupCardComponent,
+    LibraryCardComponent,
+    LibraryCardV2Component,
+    LibraryCardV3Component,
+    LibraryCardV4Component,
+    MemberCardComponent,
+    CourseCardComponent,
+    CourseCuriculumCardComponent,
+    MyCourseCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonConsumptionModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
